6a86601bb9e74f7e5f9853fe67ea343ede98e34c
bayupriyambada17 <bayupm.dev@gmail.com>
2024-06-01 16:01:09 +0700

    Fix Env Deploy

--- taskList-express/config/config.js
+++ taskList-express/config/config.js
@@ -1,10 +1,14 @@
+const { Sequelize } = require('sequelize');
+
 require('dotenv').config();
 
 const {
   DB_USERNAME,
-  DB_NAME,
+  DB_DATABASE,
   DB_PASSWORD,
-  DB_HOSTNAME
+  DB_HOST,
+  DB_PORT,
+  DB_URI
 } = process.env;
 
 
@@ -12,22 +16,55 @@ module.exports = {
   "development": {
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOSTNAME,
+    "database": DB_DATABASE,
+    "host": DB_HOST,
     "dialect": "mysql"
   },
   "test": {
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOSTNAME,
+    "database": DB_DATABASE,
+    "host": DB_HOST,
     "dialect": "mysql"
   },
   "production": {
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOSTNAME,
-    "dialect": "mysql"
+    "database": DB_DATABASE,
+    "host": DB_HOST,
+    "dialect": "mysql",
+    "port": DB_PORT || 3306, // default MySQL port
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 30000,
+      "idle": 10000
+    }
   }
 }
+
+// const sequelizeOptions = {
+//   dialect: 'mysql',
+//   define: {
+//     timestamps: true,
+//   },
+//   dialectOptions: {
+//     connectTimeout: 60000, // Set connection timeout to 60 seconds
+//   },
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
+
+// const db = DB_URI
+//   ? new Sequelize(DB_URI, sequelizeOptions)
+//   : new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+//     host: DB_HOST,
+//     port: DB_PORT || 3306, // Default MySQL port
+//     ...sequelizeOptions
+//   });
+
+// module.exports = db;
